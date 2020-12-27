690dea0c4a15126594e88f22e5d426ab86692414
COVVA <aidharrison10@gmail.com>
2020-12-27 13:39:47 +0000

    After license update

--- Library/Main/Babbage.cpp
+++ Library/Main/Babbage.cpp
@@ -2,12 +2,11 @@
 * Pass by reference for most part to improve performance and memory usage | Careful!
 * Implement variable paramater amount for functions
 * Find way to remove the need for '()' for certain functions. Low level C?
-* Use the 'static' keyword for performance reasons (RESEARCH)
 * Implement both vector and array versions of functions, with some exceptions
 * Compile into a library file along with 'Babbage.h'
 * Maybe incorporate some x86 Assembly into this for true overriding | MAJOR ADDITION
-* Classes and structs can be implemented when necessary inside namespaces but for now change most to namespace. Namespaces also allow for templates??
-* Replace headers with C when necessary, <iostream>, <vector>, <cmath> etc.
+* Classes and structs can be implemented when necessary inside namespaces but for now change most to namespace
+* Replace headers with C when necessary <vector>, <cmath> etc.
 */
 
 // Data Structures and Algorithms (Add overloads for user simplicity)
@@ -16,7 +15,7 @@
 
 int main() {
     // Babbage Main File
-    // Run setup here
+    // Run setup here if necessary
 
     return 0;
 }
--- Library/Main/Babbage.h
+++ Library/Main/Babbage.h
@@ -1,12 +1,17 @@
 #ifndef Babbage_h
 #define Babbage_h
 
+#define WAIT std::cin.get()
+
+// Add general typedefs
+
 // General non-specified header
 namespace BabbageGeneral {
     int* Asize(int array[]) {
         int size = sizeof(array) / sizeof(array[0]); // Fix
         return &size;
     }
+    extern int userInput;
 }
 
 #endif
